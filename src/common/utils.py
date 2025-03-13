74b8578caf3dbf48c75d8b2c668b3e1101df0180
Yardenl22 <Yardenl227@gmail.com>
2025-03-13 14:23:25 +0200

    bug-fix

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,12 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+def is_running_in_docker() -> bool:
+    return os.path.exists('/.dockerenv')
 
 def get_env_variable(var_name: str) -> str:
+    docker_env_vars = {
+        "REDIS_CONNECTION_STRING": "redis://redis:6379",
+        "RABBITMQ_CONNECTION_STRING": "amqp://guest:guest@rabbitmq:5672/",
+    }
+
+    if is_running_in_docker() and var_name in docker_env_vars:
+        return docker_env_vars[var_name]
+
     value = os.getenv(var_name)
     if not value:
-        raise ValueError(f'Environment variable {var_name} is missing')
+        raise ValueError(f"Environment variable {var_name} is missing")
     return value
--- src/services/master/api/internal_router.py
+++ src/services/master/api/internal_router.py
@@ -16,22 +16,24 @@ logger = CustomLogger(component='INTERNAL_API')
 
 
 @internal_router.post('/submit_task', include_in_schema=False)
-async def submit_task(hashes: list[str]):
+async def submit_task(hashes: list[str]) -> list[Task]:
     try:
         existing_results = await storage.get_results()
         existing_hashes = {entry.hash for entry in existing_results}
         new_hashes = [h for h in hashes if h not in existing_hashes]
 
         if not new_hashes:
-            return {'message': 'All hashes were already computed.', 'existing_results': existing_results}
+            return []
 
         tasks = [Task(hashes=new_hashes[i : i + CHUNK_SIZE]) for i in range(0, len(new_hashes), CHUNK_SIZE)]
 
         for task in tasks:
-            await connection_manager.redis_adapter.set_task(task.id, TaskStatusEnum.QUEUED, task.hashes)
+            await connection_manager.redis_adapter.set_task(task.id, TaskStatusEnum.PROCESSING, task.hashes)
             logger.info(f'Task {task.id} created and queued')
             await connection_manager.rabbitmq_adapter.send_task('tasks_queue', task.model_dump())
             logger.info(f'Task {task.id} send successfully to tasks_queue')
+
+        return tasks
 
     except redis.exceptions.ConnectionError as e:
         logger.error(f'Redis connection error: {str(e)}')
